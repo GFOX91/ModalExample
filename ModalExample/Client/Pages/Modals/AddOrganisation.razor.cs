50bef8e85e3fbdd1f64312f56ff241f1a9b1fc94
Gregory Fox <gregory.fox@tapiit.com>
2021-08-07 11:02:01 +0100

    Add project files.

---
+++ ModalExample/Client/Pages/Modals/AddOrganisation.razor.cs
@@ -0,0 +1,27 @@
+﻿using Blazored.Modal;
+using Blazored.Modal.Services;
+using Microsoft.AspNetCore.Components;
+using ModalExample.Shared;
+
+namespace ModalExample.Client.Pages.Modals
+{
+	public partial class AddOrganisation
+	{
+		[CascadingParameter]
+		public BlazoredModalInstance ModalInstance { get; set; }
+
+		[Parameter]
+		public Organisation Organisation { get; set; }
+
+		protected void Submit()
+		{
+			ModalInstance.CloseAsync(ModalResult.Ok(Organisation));
+		}
+
+		protected void Cancel()
+		{
+			ModalInstance.CancelAsync();
+		}
+
+	}
+}
---
+++ ModalExample/Shared/Organisation.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ModalExample.Shared
+{
+	public class Organisation
+	{
+		[Required]
+		[MaxLength(255)]
+		public string Name { get; set; }
+	}
+}
